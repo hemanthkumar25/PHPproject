443b9de61f0fe7bfcf0079c01b0fa81c86da32cf
hemanthkumar25 <hemanthkumar25@gmail.com>
2016-10-26 13:38:08 -0700

    Add files via upload

---
+++ contact1.php
@@ -0,0 +1,80 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Contact us | SocketSnipers</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="main1.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+ body
+{   top: 0;
+    overflow-x:auto;
+    overflow-y:auto;
+	background-image: url("cover.jpg");
+    background-color: #1e73bc;
+    width:100vw;
+    height: 100vw;
+	background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: top;
+    background-clip: inherit;
+    background-size: cover;
+
+}
+	  .photo {
+		float: left;
+		margin: 0.5em;
+		border: 1px solid #ccc;
+		padding: 1em;
+		font-size: 15px;
+		text-align: center;
+		overflow-x:auto;
+		overflow-y:auto;
+		
+	  }
+
+	</style>
+	</head>
+	<body> 
+			<div>
+			<center class= "logo">Socket Snipers</center>
+			       </div>
+<div>
+		<?php 
+include ("nav1.html");
+?>
+</div>
+<br><br>
+<div class="para">
+<h2 align="center"> <font size="17" color="white"><u>Meet the Team </u></font></h2><br><br>
+<font size="13" color="#FFFFF"><b>
+<p align="center">
+<table>
+  <tr>
+ <th></th>
+    <th width="600"></th>
+    <th></th>
+  </tr>
+  <tr><td> </td>
+    <td align="center" >Hemanth</td>
+    <td><a href="https://www.facebook.com/hemanthkumar25" ><img src="fb.png" height="40" width="40"></a>
+	<a href="https://in.linkedin.com/in/hemanthkumar25" ><img src="li.png" height="40" width="40"></a>
+	<a href="HemanthKumarPunnium.pdf" download><img src="resume.jpg" height="40" width="40"></a>
+	</td>
+  </tr>
+      <tr><td> </td>
+    <td align="center" >Veera</td>
+    <td><a href="https://www.facebook.com/vsvraja" ><img src="fb.png" height="40" width="40"></a>
+	<a href="https://www.linkedin.com/in/veerappanraja" ><img src="li.png" height="40" width="40"></a>
+	</td>
+  </tr>
+ </table></p><b>
+<font>
+<h2 align="center"> <font size="17" color="white"><u>Partnering Website</u></font></h2><br>
+<a href="http://veerappanraja.com/">http://veerappanraja.com/</a>
+</div>
+	</body>
+	</html>
